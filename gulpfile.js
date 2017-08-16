f46e5d570a6923bffe286ffb02d2e2041c040153
friedrichweise <hi@weise.io>
2017-08-16 16:37:48 +0200

    #318 use gulp instead of webpack

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var prefix = require('gulp-autoprefixer');
+
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+
+
+var paths = {
+	styles: './src/sass/**/*.scss',
+};
+
+gulp.task('styles', function() {
+	gulp.src(['./src/sass/afeefa.scss'])
+	//Sass conversion
+	.pipe(sass().on('error', sass.logError))
+	//autoprefixer for compability
+	.pipe(prefix({
+			browsers: ['> 1%','last 8 versions','Firefox >= 20'],
+			cascade: false
+	}))
+	//output afeefa.css
+	.pipe(gulp.dest('dist/built'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(cleanCSS())
+	//output afeefa.min.css
+	.pipe(gulp.dest('dist/built'))
+});
+
+function compile(watch) {
+	var bundler = watchify(browserify('./src/js/index.js', { debug: true }).transform(babel));
+	function rebundle() {
+		bundler.bundle()
+			.on('error', function(err) { console.error(err); this.emit('end'); })
+			.pipe(source('build.js'))
+			.pipe(buffer())
+			.pipe(sourcemaps.init({ loadMaps: true }))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest('./dist/built'));
+	}
+	if (watch) {
+		bundler.on('update', function() {
+			console.log('-> bundling...');
+			rebundle();
+		});
+	}
+	rebundle();
+}
+function watch() {
+	return compile(true);
+}
+
+
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+
+gulp.task('default', function() {
+	gulp.start('styles');
+	gulp.start('watch', function() { return watch(); });
+	gulp.watch(paths.styles, ['styles']);
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import MainView from './Views/MainView';
 
 function init() {
-	console.log('Hello From index.js');
+	console.log('Hello from index.js...');
 	const mainView = new MainView('Hello World');
 	mainView.drawText();
 	mainView.drawText();
